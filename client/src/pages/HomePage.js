b368a0671770da748cee7f111c8a33944d9238b9
lavnik-balyan <55632712+lavnik-balyan@users.noreply.github.com>
2021-12-06 08:12:09 -0500

    Create HomePage.js

---
+++ client/src/pages/HomePage.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import {
+  Table,
+  Pagination,
+  Select
+} from 'antd'
+
+import MenuBar from '../components/MenuBar';
+import { tickerFundamentals, tickerCompetitors, tickerNews, tickerInsider, tickerETF, comparingCompanies, sector,  MostGrossing, OpVsTax, 
+  ProfitGrowthPercentage, PreTaxVsTaxes } from '../fetcher'
+const { Column, ColumnGroup } = Table;
+const { Option } = Select;
+
+
+
+
+
+
+
+
+
+export default HomePage
