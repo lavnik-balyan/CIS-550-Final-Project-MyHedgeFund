8058280c4b2f4cc0bcdb807af6002ce421fbd238
lavnik-balyan <55632712+lavnik-balyan@users.noreply.github.com>
2021-12-12 13:34:14 -0500

    updated

--- client/src/components/MenuBar.js
+++ client/src/components/MenuBar.js
@@ -10,12 +10,67 @@ import {
 class MenuBar extends React.Component {
     render() {
         return(
-            <Navbar type="dark" theme="primary" expand="md">
+            <Navbar type="light" theme="lightcyan" expand="md">
         <NavbarBrand href="/">MyHedgeFund</NavbarBrand>
           <Nav navbar>
             <NavItem>
               <NavLink active href="/">
                 Home
+              </NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink active href="/tickerFundamentals">
+                Fundamentals
+              </NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink active href="/tickerCompetitors">
+              Competitors
+              </NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink active href="/tickerNews">
+                News
+              </NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink active href="/tickerInsider">
+                Insider
+              </NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink active href="/tickerETF">
+                ETF
+              </NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink active href="/comparingCompanies">
+                Comparison
+              </NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink active href="/sector">
+                Sector
+              </NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink active href="/mostGrossing">
+                MostGrossing
+              </NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink active href="/opVsTax">
+               opVsTax
+              </NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink active href="/profitGrowthPercentage">
+                Growth
+              </NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink active href="/preTaxVsTaxes">
+              preTaxVsTaxes
               </NavLink>
             </NavItem>
           </Nav>
